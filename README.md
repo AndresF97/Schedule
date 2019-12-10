029a389b7d5b9e808c2e5ae5de134d8fa9c5639b
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2019-12-09 21:59:02 -0800

    added some background color when the time changes and edited the read me

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Schedule
+## Site Picture
+![Schedule Website](https://media.licdn.com/dms/image/C4D22AQGyb2CfjtYQrA/feedshare-shrink_800/0?e=1579132800&v=beta&t=IMp9J5VLtd6zmR2wr1S85-YB6Ndpg_e_Hb2yU0Asd9o)
+
+## Summary 
+This website can be used by anybody not just office workers!!!. The website is set to change color once that hour of the day has passed,are in the current hour or open hours in your schedule.
+
+## Technologies Used
+- HTML - used to create elements on the DOM
+- CSS - styles html elements on page
+- Bootstrap - Used to create cosmitics of the website and Media inquries
+- Jquery - to create the edits in the website
+- Moment.js - was used to measure the amount of time passed 
+- Git - version control system to track changes to source code
+- GitHub - hosts repository that can be deployed to GitHub Pages
+
+
+
+## Code Snippet
+```html
+<html>
+<body>
+    <script>
+      <div class="row">
+        <div class="col-7">
+         <button type="button" class="btn btn-danger float-right btn-lg mr-4" id="clear">Clear</button> 
+          </div>
+          </div>
+
+    </script>
+</body>
+</html>
+```
+## Why this code is important
+- This migth be a simple button but it taught me that to think about what the use migth want like having a simple button to clear all of your schedule, because sometimes people plan there whole schedule but it can all change.
+## Author Links
+[LinkedIn](linkedin.com/in/andres-felipe-jimenez-ferreira-b67a35192)
+[GitHub](https://github.com/AndresF97)
--- schedule.js
+++ schedule.js
@@ -7,6 +7,7 @@ var hour_5=$("#hour-5");
 var hour_6=$("#hour-6");
 var hour_7=$("#hour-7");
 var hour_8=$("#hour-8");
+var hour_9=$("#hour-9");
 
 
 // this will be the tools that i will use to save the information inside  textarea
@@ -16,8 +17,10 @@ var hour_3A=$("#hour-3").find("textarea");
 var hour_4A=$("#hour-4").find("textarea");
 var hour_5A=$("#hour-5").find("textarea");
 var hour_6A=$("#hour-6").find("textarea");
-var hour_7A=$("#hour-7").find("textarea");;
+var hour_7A=$("#hour-7").find("textarea");
 var hour_8A=$("#hour-8").find("textarea");
+var hour_9A=$("#hour-9").find("textarea");
+
 
 
 
@@ -31,49 +34,57 @@ var hour_5bt=$("#hour-5").find(button);
 var hour_6bt=$("#hour-6").find(button);
 var hour_7bt=$("#hour-7").find(button);
 var hour_8bt=$("#hour-8").find(button);
+var hour_9bt=$("#hour-9").find(button);
 var clearbt =$("body").find("#clear");
 
-//Listeners that will listen to each event and will save seperalty
+//Listeners that will listen to each event and will save seperalty and return the 
 hour_1bt.on("click",function(){
-    var act = hour_1A.value;
+    var act = hour_1A.val();
     localStorage.setItem("Day_1",act)
+    var item = localStorage.getItem("Day_1")
+    hour_1A.append(item)
+    
 })
 hour_2bt.on("click",function(){
-    var act = hour_2A.value;
+    var act = hour_2A.val();
     console.log(act);
     localStorage.setItem("Day_2",act)
 });
 hour_3bt.on("click",function(){
-    var act = hour_3A.value;
+    var act = hour_3A.val();
     localStorage.setItem("Day_3",act);
 });
 hour_4bt.on("click",function(){
-    var act = hour_4A.value;
+    var act = hour_4A.val();
     localStorage.setItem("Day_4",act);
 });
 hour_5bt.on("click",function(){
-    var act= hour_5A.value
+    var act= hour_5A.val()
     localStorage.setItem("Day_5",act);
 });
 hour_6bt.on("click",function(){
-    var act = hour_6A.value;
+    var act = hour_6A.val();
     localStorage.setItem("Day_6",act);
 });
 hour_6bt.on("click",function(){
-    var act = hour_7A.value;
+    var act = hour_7A.val();
     localStorage.setItem("Day_7",act);
 });
 hour_7bt.on("click",function(){
-    var act = hour_7A.value;
+    var act = hour_7A.val();
     localStorage.setItem("Day_7",act)
 });
 hour_8bt.on("click",function(){
-    var act = hour_8bt.value;
+    var act = hour_8A.val();
     localStorage.setItem("Day_8",act)
+})
+hour_9bt.on("click",function(){
+    var act = hour_9A.val();
+    localStorage.setItem("Day_9",act)
 })
 
 clearbt.on("click",function(){
-    $("textarea").empty();
+    $("textarea").val(" ")
     localStorage.clear()
 })
 //setting up the date
@@ -85,7 +96,131 @@ $(document).ready(function(){
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     day.text(days[d.getDay()]+", "+ num.getDate())
 });
+//First Box schedule time set 
+var start_time=moment("9:00am","h:mma")
+var time_1 = moment("9:59am","h:mma");
+var end_1 = moment("10:00am","h:mma");
 
-//if($("#first-time") >cunrrent-time){
-    //hour_1.attr("style","background-color:grey")
-//}
+
+if(end_1.isAfter(time_1)){
+    hour_1A.attr("style","background-color: #e6e6e6");
+}else if( start_time.isBetween(time_1)){
+    hour_1A.attr("style","background-color:#ffffcc");
+}else if(start_time.isBetween(end_1)){
+    hour_1A.attr("style","background-color: #ccffcc");
+}
+
+
+//Second Box schedule time set 
+
+var start_time_2=moment("10:00am","h:mma");
+var time_2= moment("10:59am","h:mma");
+var end_2= moment("11:00am","h:mma");
+
+if(end_2.isAfter(start_time_2)){
+    hour_2A.attr("style","background-color:#e6e6e6");
+}else if(start_time_2.isBetween(time_2)){
+    hour_2A.attr("style","background-color:#ffffcc");
+}else if (start_time_2.isBetween(end_2)){
+    hour_2A.attr("style","background-color:#ccffcc");
+};
+
+//Third Box Schedule box time set 
+
+var start_time_3=moment("11:00am","h:mma");
+var time_3= moment("11:59am","h:mma");
+var end_3= moment("12:00pm","h:mma");
+
+if(end_3.isAfter(start_time_3)){
+    hour_3A.attr("style","background-color:#e6e6e6");
+}else if(start_time_3.isBetween(time_3)){
+    hour_3A.attr("style","background-color:#ffffcc");   
+}else if(start_time_3.isBetween(end_3)) {
+    hour_3A.attr("style","background-color:#ccffcc");
+};
+
+//Fourth BOx schedule Box time 
+
+
+var start_time_4=moment("12:00pm","h:mma");
+var time_4 = moment("12:59pm","h:mma");
+var end_4=moment("1:00pm","h:mma");
+
+
+if(end_4.isAfter(start_time_4)){
+    hour_4A.attr("style","background-color:#e6e6e6");
+}else if(start_time_4.isBetween(time_4)){
+    hour_4A.attr("style","background-color:#ffffcc"); 
+}else if (start_time_4.isBetween(end_4)){
+    hour_4A.attr("style","background-color:#ccffcc");
+}
+
+//Fifth Box schedule
+
+
+var start_time_5 =moment("1:00pm","h:mma");
+var time_5 =moment("1:59pm","h:mma");
+var end_5 =moment("2:00pm","h:mma");
+
+if(end_5.isAfter(start_time_5)){
+    hour_5A.attr("style","background-color:#e6e6e6");
+}else if(start_time_5.isBetween(time_5)){
+    hour_5A.attr("style","background-color:#ffffcc");
+}else if(start_time_5.isBetween(end_5)) {
+    hour_5A.attr("style","background-color:#ccffcc");
+}
+
+//Fifth Box schedule
+
+
+var start_time_6 =moment("2:00pm","h:mma");
+var time_6 =moment("2:59pm","h:mma");
+var end_6 =moment("3:00pm","h:mma");
+
+if(end_6.isAfter(start_time_6)){
+    hour_6A.attr("style","background-color:#e6e6e6");
+}else if(start_time_6.isBetween(time_6)){
+    hour_5A.attr("style","background-color:#ffffcc");
+}else if(start_time_6.isBetween(end_6)) {
+    hour_5A.attr("style","background-color:#ccffcc");
+}
+
+//seventh box schedule 
+var start_time_7 = moment("3:00pm","h:mma");
+var time_7 = moment("3:59pm","h:mma");
+var end_7 = moment("4:00pm","h:mma");
+
+if(end_7.isAfter(start_time_7)){
+    hour_7A.attr("style","background-color:#e6e6e6");
+}else if(start_time_7.isBetween(time_7)){
+    hour_5A.attr("style","background-color:#ffffcc");
+}else if (start_time_7.isBetween(end_7)){
+    hour_5A.attr("style","background-color:#ccffcc");
+}
+
+
+//eigth box schedule
+var start_time_8 = moment("4:00pm","h:mma");
+var time_8 = moment("4:59pm","h:mma");
+var end_8 = moment("5:00pm","h:mma");
+
+if(end_8.isAfter(start_time_8)){
+    hour_8A.attr("style","background-color:#e6e6e6");
+}else if(start_time_8.isBetween(time_8)){
+    hour_8A.attr("style","background-color:#ffffcc");
+}else if (start_time_8.isBetween(end_8)){
+    hour_8A.attr("style","background-color:#ccffcc");
+}
+
+//ninth box schedule 
+var start_time_9 = moment("5:00pm","h:mma");
+var time_9 = moment("5:59pm","h:mma");
+var end_9 = moment("6:00pm","h:mma");
+
+if(end_9.isAfter(start_time_9)){
+    hour_9A.attr("style","background-color:#e6e6e6");
+}else if(start_time_9.isBetween(time_9)){
+    hour_9A.attr("style","background-color:#ffffcc");
+}else if (start_time_9.isBetween(end_9)){
+    hour_9A.attr("style","background-color:#ccffcc");
+}
